6f95e403cd7cf0a0361665083915c224254b3a32
Victor <victor.onic@outlook.fr>
2021-10-22 10:42:31 +0200

    TP5 fini, et TP4 retouché

--- TP4 !/TP4.cpp
+++ TP4 !/TP4.cpp
@@ -1,18 +1,3 @@
-// --------------------
-// VICTOR ONIC TP4 BDP
-// --------------------
-// main() line 151
-// Partie 1: ligne 167
-// Partie 2: ligne 170
-// Partie 3: ligne 180
-// Partie 4: ligne 203
-// Partie 5: ligne 225
-// Partie 6: ligne 262
-// Partie 7: ligne 29
-// Partie 8: ligne 290
-// Partie 9: ligne 301
-// Partie 10: ligne 320
-
 #include <math.h>
 
 #include <cstdlib>
@@ -25,7 +10,6 @@ struct date {
   char jour[10];
   int j, m, a;
 };
-
 // Partie 7 ----------
 struct personne {
   char nom[15];
@@ -33,133 +17,14 @@ struct personne {
   date dateNaissance;
 };
 
-void coutDate(date d) {
-  cout << d.jour << " " << d.j << " " << d.m << " " << d.a << endl;
-}
-
-void coutPersonne(personne p, bool v = false) {
-  cout << p.prenom << " " << p.nom << " ";
-  if (v) coutDate(p.dateNaissance);
-  if (!v) cout << endl;
-}
-
-int whichWeekDay(int d, int m, int y) {  // 0: Dimanche, 1: Lundi, ...
-  int fDay, fMonth, fYear, fCentury, W;  // 'f' pour 'formula'
-
-  fDay = d;  // fDay
-  if (m == 1)  // fMonth
-    fMonth = 11;
-  else if (m == 2)
-    fMonth = 12;
-  else
-    fMonth = m - 2;
-  if (y > 1999) {  // fYear & fCentury
-    fYear = (fMonth == 11 || fMonth == 12) ? y - 2001 : y - 2000;
-    fCentury = 20;
-  } else {
-    fYear = (fMonth == 11 || fMonth == 12) ? y - 1901 : y - 1900;
-    fCentury = 19;
-  }
-  W = (fDay + (int)floor((2.6 * fMonth) - 0.2) - (2 * fCentury) + fYear +
-       (int)floor(fYear / 4) + (int)floor(fCentury / 4)) % 7;
-  W = W < 0 ? W + 7 : W;  // si W<0, alors W=W+7, ça arrive et c'est normal
-  return W;
-}
-
-date createNewDate(int a, int b) {  // créer une date entre les années a et b
-  int year, month, day, weekDay;
-  bool isLeap;
-  char daysList[10][10] = {
-    {"Dimanche"}, {"Lundi"}, {"Mardi"}, {"Mercredi"}, {"Jeudi"}, {"Vendredi"}, {"Samedi"}
-  };
-  date returnDate;
-
-  if (a > b) {
-    int tmp = a;
-    a = b;
-    b = tmp;
-  }
-  year = rand() % (b - a) + a + 1;  // a
-  isLeap = year % 4 == 0 xor (year % 100 == 0 xor year % 400 == 0);
-  month = rand() % 12 + 1;  // m
-  if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 ||
-      month == 10 || month == 12)  // j
-    day = rand() % 31 + 1;
-  else if (month == 2)
-    day = rand() % 28 + isLeap + 1;
-  else
-    day = rand() % 30 + 1;
-
-  strcpy(returnDate.jour, daysList[whichWeekDay(day, month, year)]);
-  returnDate.j = day;
-  returnDate.m = month;
-  returnDate.a = year;
-  return returnDate;
-}
-
-date createDateFromArray(char* T) {  // crée une date à partir d'un tableau de
-                                     // caractères T de la forme "jj/mm/aaaa"
-  char daysList[10][10] = {{"Dimanche"}, {"Lundi"},    {"Mardi"}, {"Mercredi"},
-                           {"Jeudi"},    {"Vendredi"}, {"Samedi"}};
-  date d = {
-    "",  
-    10 * (T[0] - '0') + (T[1] - '0'),
-    10 * (T[3] - '0') + (T[4] - '0'),
-    1000 * (T[6] - '0') + 100 * (T[7] - '0') + 10 * (T[8] - '0') + (T[9] - '0')
-  };
-  strcpy(d.jour, daysList[whichWeekDay(d.j, d.m, d.a)]);
-  return d;
-}
-
-// Les fonctions isPrior et isEqual sont utilisées seulement pour faciliter la
-// lecture du code.
-
-bool isPrior(date d1, date d2) { // si d1 < d2, return 1
-  return (d1.a < d2.a || (d1.m < d2.m && d1.a <= d2.a) || 
-          (d1.j < d2.j && d1.m <= d2.m && d1.a <= d2.a));
-}
-
-bool isEqual(date d1, date d2) {  // si d1 = d2, return 1
-  if (d1.a == d2.a) {
-    if (d1.m == d2.m) {
-      if (d1.j == d2.j) {
-        return true;
-      }
-    }
-  }
-  return false;
-}
-
-personne createNewPersonne() {  // créer une personne
-  char PRENOMS[30][30] = {
-    {"Ayzrael"}, {"Brivael"},   {"Ciel"},    {"Doriel"},   {"Ezekiel"},
-    {"Frigiel"}, {"Galaadiel"}, {"Haykiel"}, {"Iziel"},    {"Judikael"},
-    {"Kenael"},  {"Lydiel"},    {"Mel"},     {"Natanael"}, {"Oriel"},
-    {"Primael"}, {"Quiriel"},   {"Rigel"},   {"Samael"},   {"Tael"},
-    {"Umbriel"}, {"Viel"},      {"Wadjiel"}, {"Xarael"},   {"Yaretziel"},
-    {"Zaphkiel"}
-  };
-  char NOMS[30][30] = {
-    {"Bouboule"},      {"Bonk"},    {"Lonk"},
-    {"Biboule"},       {"Shaygai"}, {"Statu"},
-    {"Essceypet"},     {"Chien"},   {"Qelplesyr"},
-    {"Owi"},           {"Uwu"},     {"Owo"},
-    {"Star"},          {"Moon"},    {"Comet"},
-    {"Meteora"},       {"Eclipsa"}, {"Solaris"},
-    {"Byttes"},        {"Help"},    {"Leutelefaunpleurs"},
-    {"Michel"},        {"Ouahw"},   {"Aime"},
-    {"Haais"},         {"Pedet"},   {"Ayamget"},
-    {"Enndailaikitt"}, {"Biz"},     {"Feuques"}
-  };
-
-  personne returnPersonne;
-  strcpy(returnPersonne.nom, NOMS[rand() % 30]);
-  strcpy(returnPersonne.prenom, PRENOMS[rand() % 26]);
-  returnPersonne.dateNaissance = createNewDate(1000, 1500);
-
-  return returnPersonne;
-}
-
+void coutDate(date d);
+void coutPersonne(personne p);
+int whichWeekDay(int d, int m, int y);
+date createNewDate(int a, int b);
+date createDateFromArray(char T[]);
+bool isPrior(date d1, date d2);
+bool isEqual(date d1, date d2);
+personne createNewPersonne();
 
 int main() {
   srand(time(NULL));
@@ -175,7 +40,7 @@ int main() {
     cin >> N;
   } while (N < 1 || N > 998);
 
-  for (int i = 0; i < N; i++) T[i] = createNewDate(1950, 2021);
+  for (int i = 0; i < N; i++) T[i] = createNewDate(1950, 2021);  // entre 1950 et 2021
 
   // Partie 3 ----------
   char askDateChar[15] = {};
@@ -315,7 +180,7 @@ int main() {
       has2 = true;
     }
   }
-  if (!has2) cout << "Aucune.";
+  if (!has2) cout << "Aucune.\n";
 
   // Partie 10 ----------
   char personneDate1Char[15] = {};
@@ -346,4 +211,121 @@ int main() {
   if (!has3) cout << "Aucune.";
 
   return 0;
+}
+
+
+void coutDate(date d) {
+  cout << d.jour << " " << d.j << " " << d.m << " " << d.a << endl;
+}
+
+void coutPersonne(personne p) {
+  cout << p.prenom << " " << p.nom << " ";
+}
+
+int whichWeekDay(int d, int m, int y) {  // 0: Dimanche, 1: Lundi, ...
+  int fDay, fMonth, fYear, fCentury, W;  // 'f' pour 'formula'
+
+  fDay = d;  // fDay
+  if (m == 1)  // fMonth les mois commencent par 1.Mars, ...
+    fMonth = 11;
+  else if (m == 2)
+    fMonth = 12;
+  else
+    fMonth = m - 2;
+  if (y > 1999) {  // fYear & fCentury ex: 2019 -> fYear:19, fCentury:20
+    fYear = (fMonth == 11 || fMonth == 12) ? y - 2001 : y - 2000;
+    fCentury = 20;
+  } else {
+    fYear = (fMonth == 11 || fMonth == 12) ? y - 1901 : y - 1900;
+    fCentury = 19;
+  }
+  W = (fDay + (int)floor((2.6 * fMonth) - 0.2) - (2 * fCentury) + fYear +
+       (int)floor(fYear / 4) + (int)floor(fCentury / 4)) % 7;
+  W = W < 0 ? W + 7 : W;  // si W<0, alors W=W+7, ça arrive et c'est normal
+  return W;
+}
+
+date createNewDate(int a, int b) {  // créer une date entre les années a et b
+  int year, month, day, weekDay;
+  bool isLeap;
+  char daysList[10][10] = {
+    {"Dimanche"}, {"Lundi"}, {"Mardi"}, {"Mercredi"}, {"Jeudi"}, {"Vendredi"}, {"Samedi"}
+  };
+  date returnDate;
+
+  if (a > b) {
+    int tmp = a;
+    a = b;
+    b = tmp;
+  }
+  year = rand() % (b - a) + a + 1;  // a
+  isLeap = year % 4 == 0 xor (year % 100 == 0 xor year % 400 == 0);
+  month = rand() % 12 + 1;  // m
+  if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 ||
+      month == 10 || month == 12)  // j
+    day = rand() % 31 + 1;
+  else if (month == 2)
+    day = rand() % 28 + isLeap + 1;
+  else
+    day = rand() % 30 + 1;
+
+  strcpy(returnDate.jour, daysList[whichWeekDay(day, month, year)]);
+  returnDate.j = day;
+  returnDate.m = month;
+  returnDate.a = year;
+  return returnDate;
+}
+
+date createDateFromArray(char T[]) {  // crée une date à partir d'un tableau de
+                                      // caractères T de la forme "jj/mm/aaaa"
+  char daysList[10][10] = {{"Dimanche"}, {"Lundi"},    {"Mardi"}, {"Mercredi"},
+                           {"Jeudi"},    {"Vendredi"}, {"Samedi"}};
+  date d = {
+    "",  
+    10 * (T[0] - '0') + (T[1] - '0'),
+    10 * (T[3] - '0') + (T[4] - '0'),
+    1000 * (T[6] - '0') + 100 * (T[7] - '0') + 10 * (T[8] - '0') + (T[9] - '0')
+  };
+  strcpy(d.jour, daysList[whichWeekDay(d.j, d.m, d.a)]);
+  return d;
+}
+
+bool isPrior(date d1, date d2) {  // si d1 < d2, return 1
+  return (d1.a < d2.a || (d1.m < d2.m && d1.a <= d2.a) || 
+          (d1.j < d2.j && d1.m <= d2.m && d1.a <= d2.a));
+}
+
+bool isEqual(date d1, date d2) {  // si d1 = d2, return 1
+  if (d1.a == d2.a && d1.m == d2.m && d1.j == d2.j) return true;
+  return false;
+}
+
+personne createNewPersonne() {  // créer une personne
+  char PRENOMS[30][30] = {
+    {"Ayzrael"}, {"Brivael"},   {"Ciel"},    {"Doriel"},   {"Ezekiel"},
+    {"Frigiel"}, {"Galaadiel"}, {"Haykiel"}, {"Iziel"},    {"Judikael"},
+    {"Kenael"},  {"Lydiel"},    {"Mel"},     {"Natanael"}, {"Oriel"},
+    {"Primael"}, {"Quiriel"},   {"Rigel"},   {"Samael"},   {"Tael"},
+    {"Umbriel"}, {"Viel"},      {"Wadjiel"}, {"Xarael"},   {"Yaretziel"},
+    {"Zaphkiel"}
+  };
+  char NOMS[30][30] = {
+    {"Bouboule"},      {"Bonk"},    {"Lonk"},
+    {"Biboule"},       {"Shaygai"}, {"Statu"},
+    {"Essceypet"},     {"Chien"},   {"Qelplesyr"},
+    {"Owi"},           {"Uwu"},     {"Owo"},
+    {"Star"},          {"Moon"},    {"Comet"},
+    {"Meteora"},       {"Eclipsa"}, {"Solaris"},
+    {"Byttes"},        {"Help"},    {"Leutelefaunpleurs"},
+    {"Michel"},        {"Ouahw"},   {"Aime"},
+    {"Haais"},         {"Pedet"},   {"Ayamget Enndailaikitt"},
+    {"Zouzoua"},       {"Biz"},     {"Feuques"}
+  };
+
+  personne returnPersonne;
+  strcpy(returnPersonne.nom, NOMS[rand() % 30]);
+  strcpy(returnPersonne.prenom, PRENOMS[rand() % 26]);
+  returnPersonne.dateNaissance = createNewDate(1000, 1500);
+
+  return returnPersonne;
 }
